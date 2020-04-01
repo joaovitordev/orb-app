2a3c052bff29288de76b5333ee7d65995657c7c6
João Vitor <jv17fc@gmail.com>
2020-04-01 12:16:30 -0300

    Gerando páginas

--- orb/src/app/app-routing.module.ts
+++ orb/src/app/app-routing.module.ts
@@ -4,6 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'categories',
+    loadChildren: () => import('./categories/categories.module').then( m => m.CategoriesPageModule)
+  },
+  {
+    path: 'business',
+    loadChildren: () => import('./business/business.module').then( m => m.BusinessPageModule)
+  },
+  {
+    path: 'business-profile',
+    loadChildren: () => import('./business-profile/business-profile.module').then( m => m.BusinessProfilePageModule)
+  },
 ];
 
 @NgModule({
